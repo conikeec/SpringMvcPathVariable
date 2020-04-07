2ea9c4de218fd67d7b0ab0d2651b3679fb7b92b5
Andre Gott <54156675+ShiftAndre@users.noreply.github.com>
2020-04-07 15:31:35 -0500

    Added a method sanitizer for size

--- src/main/java/com/spring/mvc/Ctrl.java
+++ src/main/java/com/spring/mvc/Ctrl.java
@@ -39,15 +39,31 @@ public class Ctrl {
 
 	// Max <= 6, let's say this is an acceptable partial mitigation. 
 	@RequestMapping(value="/map/{countryName}/{userName}", method=RequestMethod.GET)
-	public String getMapDataSanitize(@Valid @Size(min=6,max=6)  @PathVariable String input, @PathVariable Map<String, String> path) {
+	public String getMapDataSize(@Valid @Size(min=6,max=6)  @PathVariable String input, @PathVariable Map<String, String> path) {
 		String country = input;  
 		return country;
 	}
 
 	// Max of 100 is > 6, so this is an unacceptable partial mitigation. 
-	@RequestMapping(value={"/map/{countryName}/{userName}"}, method={RequestMethod.GET})
-	public String getMapDataNotSanitized(@Valid @Size(min=6, max=100) @PathVariable String input, @PathVariable Map<String, String> path) {
-	String country = input;
-	return country;
-	}		
+    @RequestMapping(value={"/map/{countryName}/{userName}"}, method={RequestMethod.GET})
+    public String getMapDataNotSanitized(@Valid @Size(min=6, max=100) @PathVariable String input, @PathVariable Map<String, String> path) {
+        String country = input;
+        return country;
+    }	
+	
+	// Max of 100 is > 6, but the call uses the getSanitizedParam( ) protection. 
+    @RequestMapping(value={"/map/{countryName}/{userName}"}, method={RequestMethod.GET})
+    public String getMapDataSanitized(@Valid @Size(min=6, max=100) @PathVariable String input, @PathVariable Map<String, String> path) {
+        String country = getSanitizedParam(input);
+        return country;
+    }	
+	
+    private String getSanitizedParam(String param) {
+    	if (param.length() <= 7) {
+    		return param;
+    	} else {
+    		return ""; 
+    	}
+    }
+    
 }
